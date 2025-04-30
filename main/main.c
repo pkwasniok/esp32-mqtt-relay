08952e87625395b7f5e24a4e90293adb243f0896
Patryk Kwaśniok <kwasniokpatryk@gmail.com>
2025-04-30 18:38:31 +0200

    Added main loop

--- main/main.c
+++ main/main.c
@@ -2,20 +2,51 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_log.h"
+
+#define TAG "APP"
+
+#define STATE_SETUP   0
+#define STATE_RUNNING 1
+#define STATE_ERROR   2
 
 void app_main(void)
 {
-    int result = app_setup();
-
-    if (result == SETUP_SUCCESS) {
-        printf("Success!\n");
-    } else {
-        printf("Error!\n");
-    }
+    int state = STATE_SETUP;
 
     while (1) {
-        printf("Hello, world!\n");
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        switch (state) {
+
+            case STATE_SETUP:
+                ESP_LOGI(TAG, "Entering setup state");
+
+                if (app_setup() == SETUP_SUCCESS) {
+                    state = STATE_RUNNING; 
+                } else {
+                    state = STATE_ERROR;
+                }
+
+                break;
+
+            case STATE_RUNNING:
+                ESP_LOGI(TAG, "Entering running state");
+
+                printf("Running\n");
+                vTaskDelay(5000 / portTICK_PERIOD_MS);
+
+                break;
+
+            case STATE_ERROR:
+                ESP_LOGI(TAG, "Entering error state");
+
+                ESP_LOGE(TAG, "Restarting in 5 seconds...");
+                vTaskDelay(5000 / portTICK_PERIOD_MS);
+                esp_restart();
+
+                break;
+
+        }
     }
 }
 
